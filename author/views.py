d50b26b62d493ae5beeda226ded3e1a93818f1b0
Oleksii Mishchenko <alex3hard@gmail.com>
2025-12-28 17:54:43 +0200

    2nd blank line added

--- author/views.py
+++ author/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from author.models import Author
 from .serializers import AuthorSerializer
 
+
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
