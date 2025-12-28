015ac3104a71da044d27e0c5124c76ac44d04ded
Oleksii Mishchenko <alex3hard@gmail.com>
2025-12-28 17:49:15 +0200

    added basename manage to router.register

--- author/urls.py
+++ author/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import AuthorViewSet
 
 router = DefaultRouter()
-router.register("authors", AuthorViewSet)
+router.register("authors", AuthorViewSet, basename="manage")
 urlpatterns = [
     path("", include(router.urls))
 ]
